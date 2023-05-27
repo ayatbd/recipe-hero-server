d1db75e18596ee53a62e66c7b66668fc5166a370
ayatbd <albehestione@gmail.com>
2023-05-28 00:48:20 +0600

    updated chef picture

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const chefs = require("./chef.json");
+
 app.get('/', (req, res) => {
     res.send('server is running')
 })
+
+
+app.get("/chefs", (req, res) =>{
+    res.send(chefs);
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
